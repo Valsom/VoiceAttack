396d3b8c3abc35c6b5dd5186a629369eeb65f09b
Lucas de Jong <lv.dejong@student.avans.nl>
2021-02-19 00:21:20 +0100

    Initial release

---
+++ Valsom.VoiceAttack.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using System;
+
+using Xunit;
+
+namespace Valsom.VoiceAttack.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1() { }
+    }
+}
---
+++ Valsom.VoiceAttack/Log/VoiceAttackColor.cs
@@ -0,0 +1,16 @@
+﻿namespace Valsom.VoiceAttack.Log
+{
+    public enum VoiceAttackColor
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Orange,
+        Purple,
+        Blank,
+        Black,
+        Gray,
+        Pink
+    }
+}
